2a0c3ae583b451a06f7d0419cde00f873a3ad00b
HuynhThanhNhuanB2205999 <nhuanb2205999@student.ctu.edu.vn>
2025-07-22 15:06:29 +0700

    chore: configure App.vue as root component

--- frontend-spa/src/App.vue
+++ frontend-spa/src/App.vue
@@ -1,3 +1,23 @@
+<script setup>
+import AppHeader from '@/components/AppHeader.vue';
+// XÓA CÁC DÒNG NÀY:
+// import { useCartStore } from '@/store/cartStore';
+// const cartStore = useCartStore()
+// if (!cartStore.cartId)
+//   cartStore.fetchCartId()
+// console.log(cartStore.cartId)
+</script>
+
 <template>
-  <h1>Hello, Vue.js</h1>
+  <AppHeader />
+  <div class="container mt-0">
+    <router-view />
+  </div>
 </template>
+
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
