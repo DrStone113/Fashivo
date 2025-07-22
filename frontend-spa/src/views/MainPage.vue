c527e0861322cf66d0f0134d051e0f0b7fc848d0
HuynhThanhNhuanB2205999 <nhuanb2205999@student.ctu.edu.vn>
2025-07-22 15:05:13 +0700

    feat(view): add MainPage as homepage

---
+++ frontend-spa/src/views/MainPage.vue
@@ -0,0 +1,12 @@
+//ct313hm02-project-DrStone113/frontend-spa/src/views/MainPage.vue
+<script setup>
+import MainPageButtons from '@/components/MainPageButtons.vue';
+import MainCarousel from '@/components/MainCarousel.vue';
+</script>
+
+<template>
+  <h1 class="text-center fw-bold mt-3">WELCOME TO FASHIVO CCLOTHING</h1>
+  <MainPageButtons />
+  <hr />
+  <MainCarousel />
+</template>
